ca52b8125b816f35fd845acc174ae75d4f6991c2
priyanshi9b@gmail.com <priyanshi9b@gmail.com>
2020-11-05 10:15:00 +0530

    Updates

---
+++ precache-manifest.695da2ffd4d6a006e4add0df7365b5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d5807df250016bab5c71fe2957b3e8",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "01162a9e46c6252f8bb9",
+    "url": "/todoapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a787e84c6fb3c2bdb511",
+    "url": "/todoapp/static/js/2.c8373633.chunk.js"
+  },
+  {
+    "revision": "3b6f9a902a52e6b0f251c610a6f2d1e8",
+    "url": "/todoapp/static/js/2.c8373633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01162a9e46c6252f8bb9",
+    "url": "/todoapp/static/js/main.bf255a94.chunk.js"
+  },
+  {
+    "revision": "10a8c3fbc020010d9fe1",
+    "url": "/todoapp/static/js/runtime-main.7fd9679d.js"
+  }
+]);
